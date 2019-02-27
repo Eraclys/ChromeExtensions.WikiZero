0c4da0c93bb12fad53068c87482981a9f0493869
Eraclys <milian.lichere@gmail.com>
2019-02-27 07:15:09 +0000

    initial commit

---
+++ background.js
@@ -0,0 +1,11 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+         return {redirectUrl: details.url.replace('wikipedia', '0wikipedia')};
+    },
+    {
+		urls: [
+			"*://wikipedia.org/*",
+			"*://*.wikipedia.org/*"]
+	},    
+    ["blocking"]
+);
